ab8580a597bbef44dc9b375ecf627fec9e621472
Nguyentrongtien <tien.nguyentrong.cit24@eiu.edu.vn>
2026-05-19 20:49:09 +0700

    remove tracked node_modules

--- urent-server/api/index.ts
+++
@@ -1,3 +0,0 @@
-// NOTE: This file is deprecated.
-// Vercel deployment entry point is located at root: /api/index.ts
-// Please do not use this file for Vercel deployment.
--- urent-server/src/utils/hash.ts
+++
@@ -1,4 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export const hashPassword = (value: string) => bcrypt.hash(value, 10);
-export const comparePassword = (value: string, hashed: string) => bcrypt.compare(value, hashed);
