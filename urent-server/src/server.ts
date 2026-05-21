b4294d8f6435692011fdd71d8d64aec5e02b1a97
Nguyentrongtien <tien.nguyentrong.cit24@eiu.edu.vn>
2026-05-21 18:01:45 +0700

    feat: add workspaces for client and server, update dependencies, and improve server logging
    - Added workspaces for urent-client and urent-server in package.json
    - Updated node engine requirement to >=20 in package.json and package-lock.json
    - Added cors dependency in the root package.json
    - Updated tsx version in devDependencies for urent-server
    - Refactored import statements in server.ts for consistency
    - Enhanced logging for server startup with clearer output

--- urent-server/src/server.ts
+++ urent-server/src/server.ts
@@ -1,16 +1,16 @@
-import 'dotenv/config';
-import dns from 'node:dns';
-import { app } from './app';
-import { initializeFirebase } from './config/firebase';
+import "dotenv/config";
+import dns from "node:dns";
+import { app } from "./app";
+import { initializeFirebase } from "./config/firebase";
 
-dns.setDefaultResultOrder('ipv4first');
+dns.setDefaultResultOrder("ipv4first");
 initializeFirebase();
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
-      console.log(`\n🚀 [ SERVER ] Backend đang chạy tại http://localhost:${PORT}\n`);
-    });
-  }
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 8000;
+  console.log(
+    `  \x1b[32m➜\x1b[0m  \x1b[1m\x1b[37mAPI Local:\x1b[0m   \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`,
+  );
+}
 
 export default app;
