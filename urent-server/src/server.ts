a2c50f93b2fa316da1e293b7fa148b8599b2dc30
Nguyentrongtien <tien.nguyentrong.cit24@eiu.edu.vn>
2026-05-23 23:09:33 +0700

    fix: add app.listen for localhost

--- urent-server/src/server.ts
+++ urent-server/src/server.ts
@@ -6,11 +6,22 @@ import { initializeFirebase } from "./config/firebase";
 dns.setDefaultResultOrder("ipv4first");
 initializeFirebase();
 
+import "dotenv/config";
+import dns from "node:dns";
+import { app } from "./app";
+import { initializeFirebase } from "./config/firebase";
+
+dns.setDefaultResultOrder("ipv4first");
+initializeFirebase();
+
+const PORT = process.env.PORT || 8000;
+
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 8000;
-  console.log(
-    `  \x1b[32m➜\x1b[0m  \x1b[1m\x1b[37mAPI Local:\x1b[0m   \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`,
-  );
+  app.listen(PORT, () => {
+    console.log(
+      `  \x1b[32m➜\x1b[0m  \x1b[1m\x1b[37mAPI Local:\x1b[0m   \x1b[36mhttp://localhost:${PORT}/\x1b[0m`,
+    );
+  });
 }
 
 export default app;
